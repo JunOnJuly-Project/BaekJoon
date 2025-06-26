ed8d1deea8cd2711d78e6423442f0c985aac2d89
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-06-26 20:40:59 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public void solution() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a, b;
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a*b);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
+}
