c573243caf93988c420e28a917840534e9a36fa0
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-06-26 20:53:48 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public void solution() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String st = br.readLine();
+		
+		System.out.println(st + "??!");
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
+}
