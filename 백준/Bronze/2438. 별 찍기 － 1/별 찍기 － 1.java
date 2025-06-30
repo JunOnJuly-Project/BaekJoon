fc86c2ec0c7bad4520215f464562c80664b112b9
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-06-30 22:43:07 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 112 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+        int n = Integer.parseInt(br.readLine());
+        for (int i=1;i<=n;i++){
+            bw.write("*".repeat(i) + "\n");
+        }
+        br.close();
+        
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
