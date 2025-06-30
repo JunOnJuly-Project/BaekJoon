171a2153280dd6fe209dcbe0fd90d915d0f04663
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-06-30 22:12:22 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+    
+        int n = Integer.parseInt(br.readLine());
+        for (int i=1;i<=n;i++){
+            st = new StringTokenizer(br.readLine());
+            
+            int A, B;
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            
+            bw.write("Case #"+i+": "+(A+B)+"\n");
+        }
+        br.close();
+        
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
