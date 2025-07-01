e4ac2724d708004b67bd989d952b1324eb70b1ad
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-07-01 17:22:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 116 ms, Memory: 14640 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        char c = br.readLine().charAt(0);
+
+        bw.write(String.format("%d", (int) c));
+        br.close();
+        
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
