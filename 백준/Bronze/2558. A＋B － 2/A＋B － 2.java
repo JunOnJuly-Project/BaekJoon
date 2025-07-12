16195d0767c09fa32f3aa443ee3957908cc56e62
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-07-12 13:53:17 +0900

    [Bronze V] Title: A+B - 2, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int A, B;
+        A = Integer.parseInt(br.readLine());
+        B = Integer.parseInt(br.readLine());		
+        
+        bw.write(A+B+"");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
