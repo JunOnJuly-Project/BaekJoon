42001a70eb164a79595401245941187ebdc72495
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-07-30 23:14:01 +0900

    [Bronze V] Title: 문자열, Time: 116 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        for (int t=0;t<T;t++){
+            String input = br.readLine();
+            bw.write(input.charAt(0));
+            bw.write(input.charAt(input.length()-1) + "\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
