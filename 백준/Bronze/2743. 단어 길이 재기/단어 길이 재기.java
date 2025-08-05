cc4aacd01a128174b0e4cda868cce4096e50a1bb
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-08-05 23:11:14 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,12 +1,21 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	public void solution() throws Exception {
+        // 입력을 위한 버퍼리더
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 출력을 위한 버퍼라이터
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+        // 입력
 		String s = br.readLine();
-        System.out.println(s.length());
+        // 결과 버퍼에 입력, String 으로 변환을 위한 + "" 
+        bw.write(s.length() + "");
+        // 출력
+        bw.flush();
+        // 입출력 버퍼 닫기
+        br.close();
+        bw.close();
 	}
 	
 	public static void main(String[] args) throws Exception {
