64bf43ebc12d4476c855cd045d91ed7e875a49d2
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-08-08 08:57:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 440 ms, Memory: 88036 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -3,24 +3,28 @@ import java.util.*;
 
 public class Main{
     public void solution() throws Exception {
+        // 버퍼, 토큰화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         
+        // 입력
         int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 최솟값, 최댓값 비교을 위한 초기화
         int max = -1000001;
         int min = 1000001;
         for (int i=0;i<N;i++){
             int num = Integer.parseInt(st.nextToken());
-            
+            // 비교 및 갱신
             max = num > max ? num : max;
             min = num < min ? num : min;
         }
         
+        // 출력
         bw.write(String.format("%d %d", min, max));
         br.close();
-        
         bw.flush();
         bw.close();
     }
