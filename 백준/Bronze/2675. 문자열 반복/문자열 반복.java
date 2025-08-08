39b6b3bb9a6308fb859342233bbe7be868f91132
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-08-08 09:12:43 +0900

    [Bronze II] Title: 문자열 반복, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -3,23 +3,28 @@ import java.util.*;
 
 public class Main{
     public void solution() throws Exception {
+        // 버퍼, 토크화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         
+        // 입력
         int T = Integer.parseInt(br.readLine());
         for (int i=0;i<T;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             String S = st.nextToken();
-        
-            String result = "";
+            
+            // 문자열을 순회하며 문자 반복 출력
             for (int j=0;j<S.length();j++){
-                result += Character.toString(S.charAt(j)).repeat(N);
+                bw.write(Character.toString(S.charAt(j)).repeat(N) + "");
             }
-            bw.write(result + "\n");
+            
+            bw.write("\n");
         }
-        br.close();
         
+        // 버퍼 관리
+        br.close();
         bw.flush();
         bw.close();
     }
