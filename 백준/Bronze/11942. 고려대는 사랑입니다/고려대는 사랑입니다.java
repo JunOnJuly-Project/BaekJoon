b518570ab24e339762b32b07829ecc32042f5810
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-08-09 21:11:16 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 92 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("고려대학교");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
