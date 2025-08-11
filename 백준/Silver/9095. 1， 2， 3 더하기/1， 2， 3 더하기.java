ad49e982fb463648ee70273d9adbb9802827df02
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-08-11 09:45:38 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 108 ms, Memory: 15784 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,24 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public void solution() throws Exception {
+        // 버퍼
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
+        // 입력
         int T = Integer.parseInt(br.readLine());
+        // DP 배열
         int[] memo = new int[11];
         memo[1] = 1;
         memo[2] = 2;
         memo[3] = 4;
         
+        // 1, 2, 3 의 합으로 나타낼 때
+        // 경우의 수는 다음과 같다
+        // ex) 4의 경우
+        // 3 의 모든 경우의 맨 뒤에 1 을 붙이는 경우
+        // 2 의 모든 경우의 맨 뒤에 2 를 붙이는 경우
+        // 1 의 모든 경우의 맨 뒤에 3 을 붙이는 경우
+        // ---------------------------------------
+        // [맨 뒤에 1 을 붙이는 경우] ← memo[3]의 경우들 + 1
+        // 1+1+1+1
+        // 1+2+1
+        // 2+1+1
+        // 3+1
+
+        // [맨 뒤에 2 를 붙이는 경우] ← memo[2]의 경우들 + 2
+        // 1+1+2
+        // 2+2
+
+        //[맨 뒤에 3 을 붙이는 경우] ← memo[1]의 경우들 + 3
+        //1+3
+        // ---------------------------------------
         for (int i=4;i<11;i++) {
-        	for (int j=1;j<=3;j++) {
-        		memo[i] += memo[i-j];
-        	}
+	        memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
         }
-        
+        // 출력
         for (int i=0;i<T;i++) {
         	int n = Integer.parseInt(br.readLine());
         	bw.write(memo[n] + "\n");
