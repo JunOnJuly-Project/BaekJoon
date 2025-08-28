007962c10e0508a7b82baee270dc60025ef01d92
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-08-28 12:05:31 +0900

    [Bronze I] Title: 약수, Time: 112 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public void solution() throws Exception {
+		// 입력
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    StringTokenizer st;
+	    
+        List<Integer> nums = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
+	    
+        int max = Collections.max(nums);
+        int min = Collections.min(nums);
+        		
+	    bw.write(max * min + "");
+	    // 출력
+	    br.close();
+	    bw.flush();
+	    bw.close();
+	  }
+	  
+	  public static void main(String[] args) throws Exception {
+	      new Main().solution();
+	  }
+}
