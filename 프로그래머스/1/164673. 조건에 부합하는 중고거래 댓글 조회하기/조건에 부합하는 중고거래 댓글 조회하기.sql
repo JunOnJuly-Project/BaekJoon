25bb4349398d00e29e8e60c8d58cb2ed5de7ae56
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-09-02 16:01:34 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,4 @@
+select b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE 
+from USED_GOODS_BOARD as b join USED_GOODS_REPLY as r using(BOARD_ID)
+where year(b.CREATED_DATE) = 2022 and month(b.CREATED_DATE) = 10
+order by r.CREATED_DATE, b.TITLE asc;
