907a2c8d99efa3756a2a48cbcec7dfbafb676149
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-09-13 22:57:23 +0900

    [Bronze V] Title: 아!, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write(br.readLine().length() >= br.readLine().length() ? "go" : "no");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
