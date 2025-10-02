98fbc46a5ab940fea76e57b7f3f5c0783924d28d
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-10-02 10:25:14 +0900

    [Bronze IV] Title: 줄번호, Time: 120 ms, Memory: 16020 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public void solution() throws Exception {
+		// 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {
+			bw.write(i + ". " + br.readLine() + "\n");
+		}
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
+}
