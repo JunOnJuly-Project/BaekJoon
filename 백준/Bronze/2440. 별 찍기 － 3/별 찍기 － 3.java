424a09df6254750257732515d20656ff64e77174
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-10-05 00:33:48 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {	
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        for (int i = N; i > 0; i--) {
+        	bw.write("*".repeat(i) + "\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
