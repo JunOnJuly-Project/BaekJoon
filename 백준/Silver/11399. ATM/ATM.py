d48c3d827a7462f5bdf935b0dded911a1a27ffb7
JunOnJuly-Project <bookandpapers717@gmail.com>
2026-01-09 11:38:10 +0900

    [Silver IV] Title: ATM, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+times = sorted(list(map(int, input().split())))
+
+for i in range(1, N):
+    times[i] += times[i-1]
+
+print(sum(times))
