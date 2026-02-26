2dfd2f8464a0401457c30eadb1dc4e90b7951427
JunOnJuly-Project <bookandpapers717@gmail.com>
2026-02-26 18:18:41 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,14 @@
+select i.ID, n.FISH_NAME, i.LENGTH
+from FISH_INFO as i
+join FISH_NAME_INFO as n using (FISH_TYPE)
+where
+(
+    (i.FISH_TYPE, i.LENGTH)
+    in
+    (
+        select FISH_TYPE, max(LENGTH)
+        from FISH_INFO
+        group by FISH_TYPE
+    )
+)
+order by i.ID
